fd5c7b5b3d68bad348386043a7075c8bdf2c637a
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-08 19:59:38 +0530

    Create EntryViewPage.jsx

---
+++ src/pages/EntryViewPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EntryTable from "../components/EntryTable";
+
+const EntryViewPage = () => {
+  return (
+    <div>
+      <h2>📋 All Entries</h2>
+      <EntryTable />
+    </div>
+  );
+};
+
+export default EntryViewPage;

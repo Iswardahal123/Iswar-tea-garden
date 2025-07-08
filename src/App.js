a84c6105cfb615e24a0f26554f5d68a637ead355
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-08 14:06:17 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EntryForm from "./components/EntryForm";
+import EntryTable from "./components/EntryTable";
+
+function App() {
+  return (
+    <div>
+      <h1>Ishwar Tea Garden</h1>
+      <EntryForm />
+      <EntryTable />
+    </div>
+  );
+}
+
+export default App;

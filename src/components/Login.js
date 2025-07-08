3ab851303d50a1ec6f298b5b5003c097d6613d63
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-08 16:06:31 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import '.src/Login.css';
+import './Login.css';
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/config";

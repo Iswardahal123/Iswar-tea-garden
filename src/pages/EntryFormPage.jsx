362782afb4d587bf91a27afffc7949611d3cf98a
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-08 19:58:42 +0530

    Create EntryFormPage.jsx

---
+++ src/pages/EntryFormPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EntryForm from "../components/EntryForm";
+
+const EntryFormPage = () => {
+  return (
+    <div>
+      <h2>➕ Add New Entry</h2>
+      <EntryForm />
+    </div>
+  );
+};
+
+export default EntryFormPage;

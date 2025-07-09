a24847f988992b997363a0d7ffc1934cdcf8b73e
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-09 15:13:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import TopBar from "./components/TopBar";
 import BottomNav from "./components/BottomNav"; // ✅ Corrected import
 
 // ✅ Pages
-import EntryForm from './components/EntryForm';
+import EntryForm from './pages/EntryFormPage';
 import EntryViewPage from "./pages/EntryViewPage";
 
 function App() {

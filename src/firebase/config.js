2e85a907db84cfcecd0391aa14dee87c5c50fd0a
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-09 16:36:29 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,20 @@
-// firebase/config.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCzsdJ3TNyoTlw1IpXj4TuAupZQx71TDng",
   authDomain: "iswar-tea-garden.firebaseapp.com",
-  databaseURL: "https://iswar-tea-garden-default-rtdb.firebaseio.com",
   projectId: "iswar-tea-garden",
-  storageBucket: "iswar-tea-garden.appspot.com", // ✅ FIXED
+  storageBucket: "iswar-tea-garden.appspot.com",
   messagingSenderId: "330070107173",
   appId: "1:330070107173:web:22deb21366dcafb8394bcc",
   measurementId: "G-MJ2SJMHS22"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider(); // ✅ Added
 
-export { auth, db };
+export { auth, db, provider };

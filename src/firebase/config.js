973551c2955d756e8cd410ef01bbd74d6116d40e
Iswardahal123 <101961208+Iswardahal123@users.noreply.github.com>
2025-07-09 16:11:45 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,22 @@
+// firebase/config.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCzsdJ3TNyoTlw1IpXj4TuAupZQx71TDng",
+  authDomain: "iswar-tea-garden.firebaseapp.com",
+  databaseURL: "https://iswar-tea-garden-default-rtdb.firebaseio.com",
+  projectId: "iswar-tea-garden",
+  storageBucket: "iswar-tea-garden.appspot.com", // ✅ FIXED
+  messagingSenderId: "330070107173",
+  appId: "1:330070107173:web:22deb21366dcafb8394bcc",
+  measurementId: "G-MJ2SJMHS22"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
